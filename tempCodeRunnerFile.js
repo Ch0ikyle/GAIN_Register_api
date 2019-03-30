68fef581c50f5db9976ada8edbd441d95be3ccc1
Hwarang Choi <az8606@naver.com>
2019-03-30 21:02:22 +0900

    folder form

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,63 @@
+import Koa from 'koa';
+import Router from 'koa-router';
+
+const app = new Koa();
+const router = new Router();
+
+router.get('/', async ctx =>{
+
+    ctx.body = `<h1>Root Page</h1>
+                <br>
+                <p>TodoList - Root page</p>`;
+});
+
+router.get('/login', async ctx =>{
+    ctx.body = `<h1>Login Page</h1>
+                <br>
+                <p>TodoList - Login Page`;
+});
+
+router.get('/about', async ctx => {
+    ctx.body = `<h1>About page</h1>
+                <br>
+                <p>Todolist - About page</p>`;
+  });
+  
+  
+  
+  router.get('/todolist', async ctx => {
+  
+  
+  
+    ctx.body = `<h1>Todolist page</h1>
+  
+                <br>
+  
+                <p>Todolist - Todolist page</p>`;
+  
+  
+  
+  });
+  
+  
+  
+  router.get('/register', async ctx => {
+  
+  
+  
+    ctx.body = `<h1>Register page</h1>
+  
+                <br>
+  
+                <p>Todolist - Register page</p>`;
+  
+  
+  
+  });
+
+app.use(async ctx => {
+    ctx.body = "<h1>First koa Example !!</h1>";
+});
+
+console.log('서버 동작 중');
+app.listen(7777);
